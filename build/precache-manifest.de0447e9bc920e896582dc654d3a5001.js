16e879c08d21dccc9dc7c59d6897377a872e62bd
Rahul <kumar.rahul@iitgn.ac.in>
2023-05-16 23:20:15 +0530

    files

---
+++ build/precache-manifest.de0447e9bc920e896582dc654d3a5001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcca84c23bc26bc5b4f7",
+    "url": "/static/css/main.867bfc0c.chunk.css"
+  },
+  {
+    "revision": "bcca84c23bc26bc5b4f7",
+    "url": "/static/js/main.bcca84c2.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "65b61e77f3b9790514d2",
+    "url": "/static/js/2.65b61e77.chunk.js"
+  },
+  {
+    "revision": "7855737d81de59457bd8",
+    "url": "/static/js/runtime~main.7855737d.js"
+  },
+  {
+    "revision": "94a991576f04e21ac85479184def583a",
+    "url": "/index.html"
+  }
+];
